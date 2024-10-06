5ab968c678a96907a122fab1376da30830aacd56
chaegang01 <chaegang01@naver.com>
2024-10-06 20:52:39 +0900

    my_timetable 페이지 수정
    timetable 위젯 추가

--- dg_table/lib/screens/main_task/my_timetable.dart
+++ dg_table/lib/screens/main_task/my_timetable.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:dg_table/screens/main_task/my_timetable/timetable.dart'; // next_lecture.dart를 import
 
 class MyTimetable extends StatelessWidget {
-  @override
+    @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        margin: const EdgeInsets.all(10.0),
-        color: Colors.yellow,
-        // width: 48.0,
-        // height: 48.0,
+      child: SingleChildScrollView( // 스크롤 가능하게 변경
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Timetable(),
+          ],
+        ),
       ),
     );
   }
